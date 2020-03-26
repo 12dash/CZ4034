8ef6092342b39628efc7acccac9add798a50796d
Soham <soham.dandapath@gmail.com>
2020-03-26 13:14:56 +0800

    Results page UI improved

--- Corona_Virus/Corona_Virus/urls.py
+++ Corona_Virus/Corona_Virus/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,  name = 'home'),
     path('admin/', admin.site.urls),  
     path('dashboard/', include( 'dashboard.urls')),  
     path('search/', include ('search.urls')),
